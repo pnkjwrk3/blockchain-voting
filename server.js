f7cc4d0b102e3709048d99b9b541dc852a692b66
Pankaj Warke <pnkjwrk3@gmail.com>
2019-03-28 03:10:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,3 +15,14 @@ app.use('/users', Users)
 app.listen(port, () => {
     console.log("Server is running on port: " + port)
 })
+
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
