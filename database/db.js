22937d81e584baa8c522160d0a3e63969a3bc2ca
Pankaj Warke <pnkjwrk3@gmail.com>
2019-03-30 01:33:27 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,8 +1,8 @@
 const Sequelize = require("sequelize")
 const db = {}
 
-const sequelize = new Sequelize("nodejs_app", "root", "", {
-    host: 'localhost',
+const sequelize = new Sequelize("CSe9oMyNig", "CSe9oMyNig", "T7JPtShbuM", {
+    host: 'remotemysql.com',
     port:3306,
     dialect: 'mysql',
     operatorsAliases: false,
