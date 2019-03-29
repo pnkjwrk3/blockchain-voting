b18359303d78ada046e920e03f2340dd8af59bac
Pankaj Warke <pnkjwrk3@gmail.com>
2019-03-29 14:03:25 +0530

    Update Form.js

--- client/src/components/dashboard/Form.js
+++ client/src/components/dashboard/Form.js
@@ -10,7 +10,7 @@ class Form extends React.Component {
         <div className='form-group'>
           <label>Select Candidate</label>
           <select ref={(input) => this.candidateId = input} className='form-control'>
-            <option value="0" disabled>Select candidate </option>
+            <option value="0" disabled selected>Select candidate </option>
             {this.props.candidates.map((candidate) => {
               return <option value={candidate.id}>{candidate.name}</option>
             })}
