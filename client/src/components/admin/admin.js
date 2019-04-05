f2099f17045f16260859414f739b588eb786704d
Pankaj Warke <pnkjwrk3@gmail.com>
2019-04-06 02:23:01 +0530

    errors

--- client/src/components/admin/admin.js
+++ client/src/components/admin/admin.js
@@ -4,6 +4,10 @@ import Web3 from 'web3'
 import TruffleContract from 'truffle-contract'
 import jwt_decode from 'jwt-decode'
 
+
+
+var contractCode = "pragma solidity^0.5.0;contract Election{struct Candidate{uint id;string name;uint voteCount;} address public owner;string public electionName;uint public candidatesCount;bool public votingEnded;bool public votingStarted;mapping(uint=>Candidate)public candidates;mapping(address=>bool)public voters;modifier ownerOnly(){require(msg.sender==owner);_;} constructor(string memory _name)public{owner=msg.sender;electionName=_name;} event votedEvent(uint indexed _candidateId);function addCandidate(string memory name)ownerOnly public{require(!votingStarted);candidatesCount++;candidates[candidatesCount]=Candidate(candidatesCount,name,0);} function vote(uint _candidateId)public{require(!voters[msg.sender]);require(!votingEnded);require(votingStarted);require(!(msg.sender==owner));require(_candidateId>0&&_candidateId<=candidatesCount);voters[msg.sender]=true;candidates[_candidateId].voteCount+=1;emit votedEvent(_candidateId);} function begin()ownerOnly public{require(msg.sender==owner);votingStarted=true;} function end()ownerOnly public{require(msg.sender==owner);votingEnded=true;}}"
+
 class Admin extends React.Component {
     constructor(props) {
         super(props)
@@ -13,18 +17,32 @@ class Admin extends React.Component {
             contractadd: '0x'
         }
 
-        if (typeof window.web3 !== 'undefined') {
-            this.web3Provider = window.web3.currentProvider
-          } else {
-            this.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545')
-          }
-      
-          this.web3 = new Web3(this.web3Provider)
+        // uncomment the following code before deploy
+        // if (typeof window.web3 !== 'undefined') {
+        //     this.web3Provider = window.web3.currentProvider
+        // } else {
+        //     this.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545')
+        // }
+        this.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545') //comment this before deploy
+
+        web3 = new Web3(this.web3Provider)
     }
 
-    render(){
-        return(
-            <h1>Admin</h1>
+    deployContract = () => {
+        var compiled = web3.eth.compile.solidity(contractCode);
+
+        var code = compiled.code;
+        var abi = compiled.info.abiDefinition;
+        console.log(abi)
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Admin</h1>
+                <button onClick={this.deployContract}>deploy</button>
+            </div>
+
         )
     }
 }
