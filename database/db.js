c0a13115cf88168e174922e12fc041a02ee7218c
Pankaj Warke <pnkjwrk3@gmail.com>
2019-04-14 22:02:35 +0530

    final with new db

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize")
 const db = {}
 
-const sequelize = new Sequelize("Y4dJoErYsu", "Y4dJoErYsu", "ZqFcJRl6RG", {
+const sequelize = new Sequelize("usID8aT1ZV", "usID8aT1ZV", "jbRTYxaPSe", {
     host: 'remotemysql.com',
     port:3306,
     dialect: 'mysql',
